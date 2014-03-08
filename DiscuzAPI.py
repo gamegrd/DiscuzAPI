4ff83378868f3ec69a1460942513b7b554d1af0f
gamegrd <gamegrd@gmail.com>
2014-03-08 11:30:07 +0800

    add support portal

--- DiscuzAPI.py
+++ DiscuzAPI.py
@@ -43,7 +43,7 @@ class DiscuzAPI:
 		postData = urllib.urlencode({'username': self.userName, 'password': self.password, 'answer': '', 'cookietime': '2592000', 'handlekey': 'ls', 'questionid': '0', 'quickforward': 'yes',  'fastloginfield': 'username'})
 		req = urllib2.Request(url,postData)
 		content = urllib2.urlopen(req).read()
-		if self.userName.encode('gbk') in content:
+		if self.userName.encode('utf8') in content:
 			self.isLogon = True
 			print 'logon success!'
 			self.initFormhashXq()
@@ -54,7 +54,7 @@ class DiscuzAPI:
  
 	def initFormhashXq(self):
 		''' 获取formhash和心情 '''
-		content = urllib2.urlopen(self.forumUrl + '/plugin.php?id=dsu_paulsign:sign').read().decode('gbk')
+		content = urllib2.urlopen(self.forumUrl + '/plugin.php?id=dsu_paulsign:sign').read().decode('utf8')
 		rows = re.findall(r'<input type=\"hidden\" name=\"formhash\" value=\"(.*?)\" />', content)
 		if len(rows)!=0:
 			self.formhash = rows[0]
@@ -74,16 +74,16 @@ class DiscuzAPI:
 	def reply(self, tid, subject = u'',msg = u'支持~~~顶一下下~~嘻嘻'):
 		''' 回帖 '''
 		url = self.forumUrl + '/forum.php?mod=post&action=reply&fid=41&tid='+str(tid)+'&extra=page%3D1&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1'
-		postData = urllib.urlencode({'formhash': self.formhash, 'message': msg.encode('gbk'), 'subject': subject.encode('gbk'), 'posttime':int(time.time()) })
+		postData = urllib.urlencode({'formhash': self.formhash, 'message': msg.encode('utf8'), 'subject': subject.encode('utf8'), 'posttime':int(time.time()) })
 		req = urllib2.Request(url,postData)
-		content = urllib2.urlopen(req).read().decode('gbk')
+		content = urllib2.urlopen(req).read().decode('utf8')
 		#print content
 		if u'发布成功' in content:
 			print 'reply success!'
 		else:
 			print 'reply faild!'
  
-	def publish(self, fid, typeid, subject = u'发个帖子测试一下下，嘻嘻~~~',msg = u'发个帖子测试一下下，嘻嘻~~~', imgId = ""):
+	def publish(self, fid, subject = u'发个帖子测试一下下，嘻嘻~~~',msg = u'发个帖子测试一下下，嘻嘻~~~', imgId = ""):
 		''' 发帖 '''
 		url = self.forumUrl + '/forum.php?mod=post&action=newthread&fid='+ str(fid) +'&extra=&topicsubmit=yes'
 		"""
@@ -92,8 +92,8 @@ class DiscuzAPI:
 		"""
 		postData = urllib.urlencode(
 					{'formhash': self.formhash, 
-					'message': msg.encode('gbk'),
-					'subject': subject.encode('gbk'),
+					'message': msg.encode('utf8'),
+					'subject': subject.encode('utf8'),
 					'posttime':int(time.time()),
 					'addfeed':'1', 
 					'allownoticeauthor':'1', 
@@ -105,30 +105,73 @@ class DiscuzAPI:
 					'rushreplyto':'', 
 					'save':'', 
 					'stopfloor':'', 
-					#'typeid':typeid,
 					'attachnew[%s][description]' % imgId: "",
 					'uploadalbum':'', 
 					'usesig':'1', 
 					'wysiwyg':'0' })
 		req = urllib2.Request(url,postData)
-		content = urllib2.urlopen(req).read().decode('gbk')
+		content = urllib2.urlopen(req).read().decode('utf8')
 		#print content
-		if u"您的主题已发布" in content:
+		if u"回复" in content:
 			print 'publish success!'
 			return 1
 		else:
 			print 'publish faild!'
 			return 0
- 
+		
+	def publishPortal(self,catid = 1,subject=u"测试新闻",msg="测试消息"):
+		''' 发帖 '''
+		url = self.forumUrl + '/portal.php?mod=portalcp&ac=article&catid=%d' % catid
+		print(url)	
+		postData = urllib.urlencode(
+					{	'title': subject.encode('utf8'),
+						'highlight_style[0]': 	'',
+						'highlight_style[1]': 	'',
+						'highlight_style[2]': 	'',
+						'highlight_style[3]': 	'',
+						'htmlname': 	'',
+						'oldhtmlname': 	'',
+						'pagetitle': 	'',
+						'catid':catid,
+						'from': 	'',
+						'fromurl': 	'',
+						'dateline': 	'',
+						'from_idtype': 	'tid',
+						'from_id': 0	,
+						'id': '0',
+						'idtype': 	'tid',
+						'url': 	'',
+						'author': 	'',
+						'conver': 	'',
+						'newalbum': "".join(u'请输入相册名称').encode('utf8'),
+						'content' : msg.encode('utf8'),
+						'summary': 	'',
+						'aid': 	'',
+						'cid': 	'',
+						'attach_ids': 0,
+						'articlesubmit': 'true',
+						'formhash': self.formhash,
+						})
+		req = urllib2.Request(url,postData)
+		content = urllib2.urlopen(req).read().decode('utf8')
+		#print content
+		if u"发布文章成功" in content:
+			print 'publishPortal success!'
+			return 1
+		else:
+			print 'publishPortal faild!'
+			return 0
+		
+	
 	def sign(self,msg = u'哈哈，我来签到了！'):
 		''' 签到 '''
 		if self.isSign:
 			return
 		if self.isLogon and self.xq:
 			url = self.forumUrl + '/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1'
-			postData = urllib.urlencode({'fastreply': '1', 'formhash': self.formhash, 'qdmode': '1', 'qdxq': self.xq, 'todaysay':msg.encode('gbk') })
+			postData = urllib.urlencode({'fastreply': '1', 'formhash': self.formhash, 'qdmode': '1', 'qdxq': self.xq, 'todaysay':msg.encode('utf8') })
 			req = urllib2.Request(url,postData)
-			content = urllib2.urlopen(req).read().decode('gbk')
+			content = urllib2.urlopen(req).read().decode('utf8')
 			#print content
 			if u'签到成功' in content:
 				self.isSign = True
@@ -139,27 +182,96 @@ class DiscuzAPI:
 	def speak(self,msg = u'hah,哈哈，测试一下！'):
 		''' 发表心情 '''
 		url = self.forumUrl + '/home.php?mod=spacecp&ac=doing&handlekey=doing&inajax=1'
-		postData = urllib.urlencode({'addsubmit': '1', 'formhash': self.formhash, 'referer': 'home.php', 'spacenote': 'true', 'message':msg.encode('gbk') })
+		postData = urllib.urlencode({'addsubmit': '1', 'formhash': self.formhash, 'referer': 'home.php', 'spacenote': 'true', 'message':msg.encode('utf8') })
 		req = urllib2.Request(url,postData)
-		content = urllib2.urlopen(req).read().encode('gbk')
+		content = urllib2.urlopen(req).read().encode('utf8')
 		#print content
 		if u'操作成功' in content:
 			print 'speak success!'
 		else:
 			print 'speak faild!'
 
-	def uploadImage(self, imageData, fid=21):
+
+
+
+	def uploadPortalImage(self, imageData, catid=1,Filename="test.jpg"):
+		imageId = None
+		# get the uid and hash
+		url = self.forumUrl + "/portal.php?mod=portalcp&ac=article&catid=%d" % catid
+		data = urllib2.urlopen(url).read().decode('utf8')
+		
+		hashReg = re.compile(r'"uid" : "(\d+)", "hash":"(\w+)"', re.S)
+		hashRet = hashReg.search( data ).group(1,2)
+		uid = hashRet[0]
+		hashRet=hashRet[1]
+		
+		# Upload the image
+
+		uploadImageUrl = self.forumUrl + "/misc.php?mod=swfupload&operation=portal&action=swfupload"
+		refer = url
+		randomStr = "7dd" + ''.join( random.sample(string.ascii_lowercase + string.digits, 8) )
+		CRLF = '\r\n'
+		#BOUNDARY = mimetools.choose_boundary()
+		BOUNDARY = "---------------------------" + randomStr
+		L = []
+		
+		L.append('--' + BOUNDARY)
+		L.append("Content-Disposition: form-data; name=\"Filename\""  )
+		L.append("")
+		L.append(Filename)
+		
+		L.append('--' + BOUNDARY)
+		L.append("Content-Disposition: form-data; name=\"uid\""  )
+		L.append("")
+		L.append(uid)
+		L.append('--' + BOUNDARY)
+		L.append('Content-Disposition: form-data; name=\"hash\"')
+		L.append("")
+		L.append(hashRet)
+		
+		L.append('--' + BOUNDARY)
+		L.append("Content-Disposition: form-data; name=\"catid\""  )
+		L.append("")
+		L.append(catid)
+				
+		
+		L.append('--' + BOUNDARY)
+		L.append('Content-Disposition: form-data; name=\"Filedata\"; filename=\"%s\"'% Filename)
+		L.append("Content-Type: image/pjpeg")
+		L.append("")
+		L.append( imageData )
+		L.append('--' + BOUNDARY + '--')
+		L.append("")
+		postData = CRLF.join(str(a) for a in L)
+
+		#print postData
+
+		req = urllib2.Request(uploadImageUrl, postData) 
+		req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % BOUNDARY )
+		req.add_header('Content-Length',  len(postData) )
+		req.add_header('Referer', refer )
+		resp = urllib2.urlopen(req)
+		body = resp.read().decode('utf-8')
+		#print(body)
+		hashReg = re.compile(r'"aid":(?P<aid>\d+).+"smallimg":"(?P<smallimg>.+)", "bigimg":"(?P<bigimg>.+)", "errorcode"', re.S)
+		hashRet = hashReg.search( body ).groupdict()
+		if hashRet:
+			return hashRet 
+		else:
+			return None
+	
+	def uploadForumImage(self, imageData, fid=1):
 		imageId = None
 		# get the uid and hash
 		url = self.forumUrl + "/forum.php?mod=post&action=newthread&fid=%d&extra=" % fid
-		data = urllib2.urlopen(url).read().decode('gbk')
+		data = urllib2.urlopen(url).read().decode('utf8')
 		hashReg = re.compile(r"<input type=\"hidden\" name=\"hash\" value=\"(.*?)\">", re.S)
 		uidReg = re.compile(r"discuz_uid = '(.*?)'", re.S)
 		hashRet = hashReg.search( data ).group(1)
 		uid = uidReg.search( data ).group(1)
 
 		# Upload the image
-		uploadImageUrl = self.forumUrl + "/misc.php?mod=swfupload&operation=upload&simple=1&type=image"
+		uploadImageUrl = self.forumUrl + "/misc.php?mod=swfupload&operation=upload&type=image"
 		refer = self.forumUrl + "/forum.php?mod=post&action=newthread&fid=%d&extra=" % fid
 		randomStr = "7dd" + ''.join( random.sample(string.ascii_lowercase + string.digits, 8) )
 		CRLF = '\r\n'
@@ -191,6 +303,7 @@ class DiscuzAPI:
 		req.add_header('Referer', refer )
 		resp = urllib2.urlopen(req)
 		body = resp.read().decode('utf-8')
+		print(body)
 		bodySp = body.split('|')
 		if len(bodySp) == 0:
 			return None
@@ -205,6 +318,8 @@ if __name__ == '__main__':
 	#robot.speak()
 	#robot.publish(21,36)
 	#robot.reply(10)
-	imageData = open('test.jpg', 'rb').read()
+	imageData = open('D:\\FRAMES_X8.jpg', 'rb').read()
 	print robot.uploadImage( imageData )
+
+
 
